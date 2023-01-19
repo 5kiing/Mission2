2eca7a921f7cc262a1d14b80016916e1f9f48f8d
5kiing <103718596+5kiing@users.noreply.github.com>
2023-01-18 18:02:39 -0700

    Add project files.

---
+++ Mission2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create variables
+            Random r = new Random();
+
+            int[] rollTotals = new int[13];
+            int timesThrown = 0;
+
+            // roll total range
+            rollTotals[0] = r.Next(6);
+
+            // prompt user
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.WriteLine("How many dice rolls would you like to simulate?");
+
+            // receive user input
+            timesThrown = Convert.ToInt32(Console.ReadLine());
+            
+            // simulate roll of the die
+            for (int i = 0; i < timesThrown; i++)
+            {
+                int roll1 = r.Next(1,7);
+                int roll2 = r.Next(1,7);
+
+                int rollTot = roll1 + roll2;
+
+                rollTotals[rollTot]++;
+            }
+
+            //print roll results
+            for (int i = 2; i <= 12; i++)
+            {
+                int percentage = (int)((double)rollTotals[i] / timesThrown * 100);
+                Console.Write(i + ": ");
+                for (int x = 0; x < percentage; x++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
